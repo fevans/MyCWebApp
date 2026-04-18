edb8b1aeef068948e7ea5592dde031cfa4467b43
Francis Evans <francis.evans@gmail.com>
2026-04-18 02:35:13 +0100

    Refactored `ProgramTests` to use a custom `WebApplicationFactory` with configurable content root.

--- MyCWebApp.Tests/ProgramTests.cs
+++ MyCWebApp.Tests/ProgramTests.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.IO;
 
 namespace MyCWebApp.Tests;
 
-public class ProgramTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.UseContentRoot(Directory.GetCurrentDirectory());
+    }
+}
+
+public class ProgramTests(CustomWebApplicationFactory factory) : IClassFixture<CustomWebApplicationFactory>
 {
     [Fact]
     public async Task GetRootEndpoint_ReturnsHelloWorld()
@@ -33,4 +43,3 @@ public class ProgramTests(WebApplicationFactory<Program> factory) : IClassFixtur
         Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
     }
 }
-    
